deb44cabaea4437c87b923af4a689800c2fb329a
zhanghao <13930333639@163.com>
2024-04-13 21:45:57 +0800

    添加数据，下载模型，训练

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
-print('hello word')
+from huggingface_hub import snapshot_download
+from datasets import load_dataset, load_from_disk
+
+# 下载模型
+snapshot_download("THUDM/chatglm3-6b", local_dir="/root/autodl-tmp/chatglm3-6b", local_dir_use_symlinks=False)
+
+# 下载数据集
+# dataset = load_dataset("YeungNLP/firefly-train-1.1M")
+# dataset.save_to_disk("./dataset/Salesforce/dialogstudio") # 保存到该目录下
+# dataset = load_from_disk("./dataset/Salesforce/dialogstudio") # 加载保存的数据集
+# print(len(dataset['train']))
+# print(dataset['train'])
+# print(dataset['train'][1]['kind'])
+# with open('./dataset/data.json', 'w', encoding='utf-8') as fp:
+
+# 按照模型要的结构组合数据
--- main.sh
+++ main.sh
@@ -2,4 +2,28 @@
 
 set -ex
 
-python demo.py
+LR=2e-4
+MAX_SEQ_LEN=3072
+
+DATESTR=`date +%Y%m%d-%H%M%S`
+RUN_NAME=hotel_lora
+OUTPUT_DIR=output/${RUN_NAME}-${DATESTR}
+mkdir -p $OUTPUT_DIR
+
+CUDA_VISIBLE_DEVICES=1 python main.py \
+    --do_train \
+    --do_eval \
+    --train_file ../data/train.chatglm3.jsonl \
+    --validation_file ../data/dev.chatglm3.jsonl \
+    --max_seq_length $MAX_SEQ_LEN \
+    --preprocessing_num_workers 1 \
+    --output_dir $OUTPUT_DIR \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --num_train_epochs 4 \
+    --evaluation_strategy steps \
+    --eval_steps 300 \
+    --logging_steps 300 \
+    --logging_dir $OUTPUT_DIR/logs \
+    --save_steps 300 \
+    --learning_rate $LR \
