8cc4af27b34c6b648d3ac8b5fe0079e6f936af5e
zhanghao <13930333639@163.com>
2024-04-13 23:33:11 +0800

    删除一些参数

--- main.sh
+++ main.sh
@@ -10,13 +10,14 @@ RUN_NAME=hotel_lora
 OUTPUT_DIR=output/${RUN_NAME}-${DATESTR}
 mkdir -p $OUTPUT_DIR
 
+# --train_file ../data/train.chatglm3.jsonl \
+# --validation_file ../data/dev.chatglm3.jsonl \
+# --max_seq_length $MAX_SEQ_LEN \
+# --preprocessing_num_workers 1 \
+
 CUDA_VISIBLE_DEVICES=1 python main.py \
     --do_train \
     --do_eval \
-    --train_file ../data/train.chatglm3.jsonl \
-    --validation_file ../data/dev.chatglm3.jsonl \
-    --max_seq_length $MAX_SEQ_LEN \
-    --preprocessing_num_workers 1 \
     --output_dir $OUTPUT_DIR \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
